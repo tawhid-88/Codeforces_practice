82a9fa34c0c0eb71b0899d7a66d1b94f853b810d
Tawhid Hasan <tawhidhasan88@gmail.com>
2025-08-18 23:14:11 +0600

    New codes added

---
+++ 486A_Calculating_Function.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    long long n; 
+        cin >> n;
+
+    long long sum = 0;
+
+    if (n % 2 == 0)
+    {
+        sum = n / 2;
+    }
+    else
+    {
+        sum = -(n + 1) / 2;
+    }
+
+        cout << sum;
+
+    return 0;
+}
