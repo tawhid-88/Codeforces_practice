0ce95d48c13cea766f6bd9e0066a4052d92e6cae
Tawhid Hasan <tawhidhasan88@gmail.com>
2025-08-28 03:56:31 +0600

    3:56AM 28th August 2025

---
+++ 785A_Anton_and_Polyhedrons.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    string s;
+    int count = 0;
+    while(n--)
+    {
+        cin >> s;
+        if (s == "Tetrahedron")
+        {
+            count += 4;
+            
+        }
+        else if (s == "Cube")
+        {
+            count += 6;
+            
+        }
+        else if (s == "Octahedron")
+        {
+            count += 8;
+        }
+        else if (s == "Dodecahedron")
+        {
+            count += 12;
+        }
+        else if(s== "Icosahedron")
+        {
+            count += 20;
+        }
+    }
+    cout << count;
+    return 0;
+}
