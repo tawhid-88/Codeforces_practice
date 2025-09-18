b1cf65bb7a83164beac8a22c99eabb66036a91e9
Tawhid Hasan <tawhidhasan88@gmail.com>
2025-09-18 14:14:49 +0600

    September 18 02:14 PM

---
+++ 96A_Football.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string s;
+    cin >> s;
+    int count = 1;
+    for (int i = 1; i <= s.length(); i++)
+    {
+        if(s[i]==s[i-1])
+        {
+            count++;
+        }
+        else{
+            count = 1;
+        }
+        if (count>=7)
+        {
+            cout << "YES";
+            return 0;
+        }
+        
+    }
+    cout << "NO";
+    return 0;
+}
